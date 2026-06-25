8bbde0554304eab7cef65669fdee133777d3b60e
Haseeb <ah085904@gmail.com>
2026-06-25 16:51:18 +0500

    convert the entry into an object

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-	entry: "./src/index.js",
+	entry: {
+		app: "./src/index.js",
+	},
 	output: {
 		filename: "[name].bundle.js",
 		path: path.resolve(__dirname, "dist"),
