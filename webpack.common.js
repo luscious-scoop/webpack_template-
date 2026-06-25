4961f8dc85baccd115bbf8f0d0f6e2c56994eec7
Haseeb <ah085904@gmail.com>
2026-06-25 16:50:31 +0500

    import file url path

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,16 @@
 // webpack.config.js
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
 	entry: "./src/index.js",
 	output: {
-		filename: "main.js",
-		path: path.resolve(import.meta.dirname, "dist"),
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
 		clean: true,
 	},
 	plugins: [
